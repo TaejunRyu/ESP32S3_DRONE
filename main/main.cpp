9edc957e8d73257d953474b6ecf4ae7f2e3300d5
taejun Ryu <ryuwillow@gmail.com>
2026-05-15 07:17:24 +0000

    시작 준비 완료!

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
+#include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 extern "C"  void app_main(void)
 {
 
+    ESP_LOGI("MAIN","시작....");
+
+    while(true){
+
+
+        vTaskDelay( pdMS_TO_TICKS(1000));
+    }
 
 }
